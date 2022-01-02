89a9fbac52a0e90ab182a957b7c9c6843ea04628
Uirá Caiado <ucaiado@yahoo.com.br>
2022-01-02 02:37:58 -0300

    chore: include py version of notebooks using jupytext

---
+++ notebooks/churn_notebook.py
@@ -0,0 +1,265 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+import shap
+import joblib
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+
+from sklearn.preprocessing import normalize
+from sklearn.model_selection import train_test_split
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+
+from sklearn.metrics import plot_roc_curve, classification_report
+# -
+
+df = pd.read_csv(r"../data/bank_data.csv")
+df.head()
+
+df.shape
+
+df.isnull().sum()
+
+df.describe()
+
+# +
+cat_columns = [
+    'Gender',
+    'Education_Level',
+    'Marital_Status',
+    'Income_Category',
+    'Card_Category'                
+]
+
+quant_columns = [
+    'Customer_Age',
+    'Dependent_count', 
+    'Months_on_book',
+    'Total_Relationship_Count', 
+    'Months_Inactive_12_mon',
+    'Contacts_Count_12_mon', 
+    'Credit_Limit', 
+    'Total_Revolving_Bal',
+    'Avg_Open_To_Buy', 
+    'Total_Amt_Chng_Q4_Q1', 
+    'Total_Trans_Amt',
+    'Total_Trans_Ct', 
+    'Total_Ct_Chng_Q4_Q1', 
+    'Avg_Utilization_Ratio'
+]
+# -
+
+df['Churn'] = df['Attrition_Flag'].apply(lambda val: 0 if val == "Existing Customer" else 1)
+
+plt.figure(figsize=(20,10)) 
+df['Churn'].hist();
+
+plt.figure(figsize=(20,10)) 
+df['Customer_Age'].hist();
+
+plt.figure(figsize=(20,10)) 
+df.Marital_Status.value_counts('normalize').plot(kind='bar');
+
+plt.figure(figsize=(20,10)) 
+sns.distplot(df['Total_Trans_Ct']);
+
+plt.figure(figsize=(20,10)) 
+sns.heatmap(df.corr(), annot=False, cmap='Dark2_r', linewidths = 2)
+plt.show()
+
+y = df['Churn']
+
+X = pd.DataFrame()
+
+# +
+# gender encoded column
+gender_lst = []
+gender_groups = df.groupby('Gender').mean()['Churn']
+
+for val in df['Gender']:
+    gender_lst.append(gender_groups.loc[val])
+
+df['Gender_Churn'] = gender_lst    
+#education encoded column
+edu_lst = []
+edu_groups = df.groupby('Education_Level').mean()['Churn']
+
+for val in df['Education_Level']:
+    edu_lst.append(edu_groups.loc[val])
+
+df['Education_Level_Churn'] = edu_lst
+
+#marital encoded column
+marital_lst = []
+marital_groups = df.groupby('Marital_Status').mean()['Churn']
+
+for val in df['Marital_Status']:
+    marital_lst.append(marital_groups.loc[val])
+
+df['Marital_Status_Churn'] = marital_lst
+
+#income encoded column
+income_lst = []
+income_groups = df.groupby('Income_Category').mean()['Churn']
+
+for val in df['Income_Category']:
+    income_lst.append(income_groups.loc[val])
+
+df['Income_Category_Churn'] = income_lst
+
+#card encoded column
+card_lst = []
+card_groups = df.groupby('Card_Category').mean()['Churn']
+ 
+for val in df['Card_Category']:
+    card_lst.append(card_groups.loc[val])
+
+df['Card_Category_Churn'] = card_lst
+    
+    
+
+# +
+keep_cols = ['Customer_Age', 'Dependent_count', 'Months_on_book',
+             'Total_Relationship_Count', 'Months_Inactive_12_mon',
+             'Contacts_Count_12_mon', 'Credit_Limit', 'Total_Revolving_Bal',
+             'Avg_Open_To_Buy', 'Total_Amt_Chng_Q4_Q1', 'Total_Trans_Amt',
+             'Total_Trans_Ct', 'Total_Ct_Chng_Q4_Q1', 'Avg_Utilization_Ratio',
+             'Gender_Churn', 'Education_Level_Churn', 'Marital_Status_Churn', 
+             'Income_Category_Churn', 'Card_Category_Churn']
+
+X[keep_cols] = df[keep_cols]
+# -
+
+X.head()
+
+# +
+# %%time
+# train test split 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.3, random_state=42)
+
+# grid search
+rfc = RandomForestClassifier(random_state=42)
+lrc = LogisticRegression(solver='liblinear')
+
+param_grid = { 
+    'n_estimators': [200, 500],
+    'max_features': ['auto', 'sqrt'],
+    'max_depth' : [4,5,100],
+    'criterion' :['gini', 'entropy']
+}
+
+cv_rfc = GridSearchCV(estimator=rfc, param_grid=param_grid, cv=5)
+cv_rfc.fit(X_train, y_train)
+
+lrc.fit(X_train, y_train)
+
+y_train_preds_rf = cv_rfc.best_estimator_.predict(X_train)
+y_test_preds_rf = cv_rfc.best_estimator_.predict(X_test)
+
+y_train_preds_lr = lrc.predict(X_train)
+y_test_preds_lr = lrc.predict(X_test)
+
+# scores
+print('random forest results')
+print('test results')
+print(classification_report(y_test, y_test_preds_rf))
+print('train results')
+print(classification_report(y_train, y_train_preds_rf))
+
+print('logistic regression results')
+print('test results')
+print(classification_report(y_test, y_test_preds_lr))
+print('train results')
+print(classification_report(y_train, y_train_preds_lr))
+# -
+
+
+
+
+
+lrc_plot = plot_roc_curve(lrc, X_test, y_test)
+
+# plots
+plt.figure(figsize=(15, 8))
+ax = plt.gca()
+rfc_disp = plot_roc_curve(cv_rfc.best_estimator_, X_test, y_test, ax=ax, alpha=0.8)
+lrc_plot.plot(ax=ax, alpha=0.8)
+plt.show()
+
+# save best model
+joblib.dump(cv_rfc.best_estimator_, '../models/rfc_model.pkl')
+joblib.dump(lrc, '../models/logistic_model.pkl')
+
+rfc_model = joblib.load('../models/rfc_model.pkl')
+lr_model = joblib.load('../models/logistic_model.pkl')
+
+lrc_plot = plot_roc_curve(lr_model, X_test, y_test)
+
+plt.figure(figsize=(15, 8))
+ax = plt.gca()
+rfc_disp = plot_roc_curve(rfc_model, X_test, y_test, ax=ax, alpha=0.8)
+lrc_plot.plot(ax=ax, alpha=0.8)
+plt.show()
+
+explainer = shap.TreeExplainer(cv_rfc.best_estimator_)
+shap_values = explainer.shap_values(X_test)
+shap.summary_plot(shap_values, X_test, plot_type="bar")
+
+# +
+# Calculate feature importances
+importances = cv_rfc.best_estimator_.feature_importances_
+# Sort feature importances in descending order
+indices = np.argsort(importances)[::-1]
+
+# Rearrange feature names so they match the sorted feature importances
+names = [X.columns[i] for i in indices]
+
+# Create plot
+plt.figure(figsize=(20,5))
+
+# Create plot title
+plt.title("Feature Importance")
+plt.ylabel('Importance')
+
+# Add bars
+plt.bar(range(X.shape[1]), importances[indices])
+
+# Add feature names as x-axis labels
+plt.xticks(range(X.shape[1]), names, rotation=90);
+# -
+
+plt.rc('figure', figsize=(5, 5))
+#plt.text(0.01, 0.05, str(model.summary()), {'fontsize': 12}) old approach
+plt.text(0.01, 1.25, str('Random Forest Train'), {'fontsize': 10}, fontproperties = 'monospace')
+plt.text(0.01, 0.05, str(classification_report(y_test, y_test_preds_rf)), {'fontsize': 10}, fontproperties = 'monospace') # approach improved by OP -> monospace!
+plt.text(0.01, 0.6, str('Random Forest Test'), {'fontsize': 10}, fontproperties = 'monospace')
+plt.text(0.01, 0.7, str(classification_report(y_train, y_train_preds_rf)), {'fontsize': 10}, fontproperties = 'monospace') # approach improved by OP -> monospace!
+plt.axis('off');
+
+
+plt.rc('figure', figsize=(5, 5))
+plt.text(0.01, 1.25, str('Logistic Regression Train'), {'fontsize': 10}, fontproperties = 'monospace')
+plt.text(0.01, 0.05, str(classification_report(y_train, y_train_preds_lr)), {'fontsize': 10}, fontproperties = 'monospace') # approach improved by OP -> monospace!
+plt.text(0.01, 0.6, str('Logistic Regression Test'), {'fontsize': 10}, fontproperties = 'monospace')
+plt.text(0.01, 0.7, str(classification_report(y_test, y_test_preds_lr)), {'fontsize': 10}, fontproperties = 'monospace') # approach improved by OP -> monospace!
+plt.axis('off');
+
+
