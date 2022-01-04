d40b88a236ca0b7d9a71548958562ebef4bbde67
Uirá Caiado <ucaiado@yahoo.com.br>
2022-01-04 02:11:38 -0300

    feat: implement import data function and test

--- churn_script_logging_and_tests.py
+++ churn_script_logging_and_tests.py
@@ -1,58 +1,74 @@
-import os
-import logging
-import churn_library_solution as cls
+'''
+Implement tests to clasees and functions implemented in churn_library
 
+Author: ucaiado
+Date: January 4th, 2022
+'''
+import os
+import sys
+import logging
+import churn_library as cls
+
+
+# set logging to write a file and to stodout
 logging.basicConfig(
     filename='./logs/churn_library.log',
     level = logging.INFO,
     filemode='w',
     format='%(name)s - %(levelname)s - %(message)s')
 
-def test_import(import_data):
-	'''
-	test data import - this example is completed for you to assist with the other test functions
-	'''
-	try:
-		df = import_data("./data/bank_data.csv")
-		logging.info("Testing import_data: SUCCESS")
-	except FileNotFoundError as err:
-		logging.error("Testing import_eda: The file wasn't found")
-		raise err
+logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 
-	try:
-		assert df.shape[0] > 0
-		assert df.shape[1] > 0
-	except AssertionError as err:
-		logging.error("Testing import_data: The file doesn't appear to have rows and columns")
-		raise err
+
+def test_import(import_data: object) -> None:
+    '''
+    test data import - this example is completed for you to assist with the
+    other test functions
+    '''
+    try:
+        df = import_data("./data/bank_data.csv")
+        logging.info("Testing import_data: SUCCESS")
+    except FileNotFoundError as err:
+        logging.error("Testing import_eda: The file wasn't found")
+        raise err
+
+    try:
+        assert df.shape[0] > 0
+        assert df.shape[1] > 0
+    except AssertionError as err:
+        logging.error(
+            "Testing import_data: The file doesn't appear "
+            "to have rows and columns")
+        raise err
 
 
 def test_eda(perform_eda):
-	'''
-	test perform eda function
-	'''
+    '''
+    test perform eda function
+    '''
+    pass
 
 
 def test_encoder_helper(encoder_helper):
-	'''
-	test encoder helper
-	'''
-
+    '''
+    test encoder helper
+    '''
+    pass
 
 def test_perform_feature_engineering(perform_feature_engineering):
-	'''
-	test perform_feature_engineering
-	'''
-
+    '''
+    test perform_feature_engineering
+    '''
+    pass
 
 def test_train_models(train_models):
-	'''
-	test train_models
-	'''
-
+    '''
+    test train_models
+    '''
+    pass
 
 if __name__ == "__main__":
-	pass
+    test_import(cls.import_data)
 
 
 
